918baad3b528ab57f2a171a125eea72c6fee9ab6
PRobles_dwec <probles19088@iesjoanramis.org>
2022-12-01 17:28:44 +0100

    -m subiendo todo

---
+++ UD3/Strings/script.js
@@ -0,0 +1,14 @@
+let text = "Boquepasa Requete 123";
+
+// console.log(text.length);
+// console.log(text.lastIndexOf("que"));
+// console.log(text.search("pasa"));
+// console.log(text.slice(text.lastIndexOf(" "), text.length -1 ));
+// console.log(text.substr(5, 3));
+//console.log(text.replaceAll("que", "Ana"));
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+ prova = ["Uno", "Dos", "Tres"];
+// console.log(text.concat(...prova));
+//console.log(text.split("e", 3));
+console.log(text.repeat(666));
