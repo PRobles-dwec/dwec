72a4c05e7116b726628530e7979c24063dc314f9
PRobles_dwec <probles19088@iesjoanramis.org>
2022-12-05 17:19:09 +0100

    cambios

--- UD4/validador-formulario_practica/script.js
+++ UD4/validador-formulario_practica/script.js
@@ -8,46 +8,74 @@ const email = document.getElementById("email");
 function mostraError(input, missatge) {
     const formcontrol = input.parentElement;
     formcontrol.className = 'form-control error';
-    const label = formcontrol.querySelector('label');
+    // const label = formcontrol.querySelector('label');
     const small = formcontrol.querySelector('small');
-    small.innerText = label.innerText + ' ' + missatge;
+    // small.innerText = label.innerText + ' ' + missatge;
+    small.innerText =  missatge;
 }
 
 function mostraCorrecte(input) {
     const formcontrol = input.parentElement;
     formcontrol.className = 'form-control correcto';
-
 }
 
-function esEmailValid(email) {
+function prenNomInput(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function esEmailValid(input) {
+    
     const re =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase()); //true, false
+    if(re.test(String(input.value.trim()))) {
+        mostraCorrecte(input);
+    } else {
+        let missatge = `${prenNomInput(input)} no té el format correcte`;
+        mostraError(input, missatge);
+    }
+}
+
+function esObligatori(inputArray) {
+    inputArray.forEach(function(input) { 
+    if(input.value.trim() === '') {
+        let missatge = `${prenNomInput(input)} és obligatori`;
+        mostraError(input, missatge);
+        prenNomInput(input);
+    }
+    else{
+        mostraCorrecte(input);
+    }
+  }); 
+}
+
+function comprovaLongitud(input, min, max) {
+    if(input.value.length < min ) {
+        let missatge = `${prenNomInput(input)} ha de tenir almenys ${min} caràcters`;
+        mostraError(input, missatge);
+    } else if (input.value.length > max ) {
+        let missatge = `${prenNomInput(input)} ha de tenir almenys ${max} caràcters`;
+        mostraError(input, missatge); 
+    } else {
+        mostraCorrecte(input);
+    }
+}
+
+function comprovaContrasenyesSonIguals(input1, input2) {
+    if(input1.value != input2.value) {
+        let missatge = `${prenNomInput(input2)} ha de ser igual que ${prenNomInput(input1)}`;
+        mostraError(input2, missatge);
+    }
 }
 
 //Event Listeners
 form.addEventListener("submit", function(e) {
     e.preventDefault();    
-
-    if(nomusuari.value === '') {
-        mostraError(nomusuari, 'és obligatori');
-    } else {
-        mostraCorrecte(nomusuari);
-    }
-
-    if(email.value === '') {
-        mostraError(email, 'és obligatori');
-    }else if (!esEmailValid(email.value)){
-        mostraError(email, 'no és vàlid');
-    }else {
-        mostraCorrecte(email);
-    }
-
-
-    if(password2.value === '') {
-        mostraError(password2, 'és obligatori');
-    }else {
-        mostraCorrecte(password2);
-    }
-
     
+    esObligatori([nomusuari, email, password, password2]);
+
+    comprovaLongitud(nomusuari, 3, 15);
+    comprovaLongitud(password, 6, 25);
+
+    esEmailValid(email);
+
+    comprovaContrasenyesSonIguals(password, password2);
 });
