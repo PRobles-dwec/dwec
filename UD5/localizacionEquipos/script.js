c6f2733075dc923430b3a771d82858efe6bbb5a0
PRobles_dwec <probles19088@iesjoanramis.org>
2022-12-19 17:01:29 +0100

    unidad 5

---
+++ UD5/localizacionEquipos/script.js
@@ -0,0 +1,21 @@
+const equipo = document.getElementById("equipo");
+const sugerencia = document.getElementById("sugerencia");
+
+//Eventos
+equipo.addEventListener("keyup", (e)=>{
+    let str = e.target.value;
+
+    if(str.length == 0) {
+        sugerencia.innerHTML = "";
+        return;
+    } else {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if(e.target.readyState == 4 && e.target.status == 200) { //El reayState = 4 significa que está completada, y el status = 200 es una negación de que no lo ha encontrado.
+                sugerencia.innerHTML = e.target.responseText;
+            }
+        };
+        xmlhttp.open("GET", );
+    }
+
+})
