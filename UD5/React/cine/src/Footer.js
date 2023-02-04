58e2e4873f4b32f2116a65dd8b75c5dfbfc0db26
PRobles-dwec <118468238+PRobles-dwec@users.noreply.github.com>
2023-02-04 13:59:12 +0100

    Añadiendo Footer

---
+++ UD5/React/cine/src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <p>Página creada por: Pau Robles Mus</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
