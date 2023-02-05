64a8d7f36179bdea4dd541c6c19ad0738c5705f6
PRobles-dwec <118468238+PRobles-dwec@users.noreply.github.com>
2023-02-05 13:02:27 +0100

    evento click por todo

--- UD5/React/cine/src/Pantalla.js
+++ UD5/React/cine/src/Pantalla.js
@@ -1,66 +1,73 @@
+import React, { useState } from "react";
+
 const Pantalla = () => {
+    const [activa, setActiva] = useState(false);
+   
+    const handleClick= () => {
+        setActiva(!activa);
+    }
     return ( 
         <div className="contenidor"> 
             <div className="pantalla"> </div>
                 <div className="fila">
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
                     <div className="seient ocupat"> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient ocupat"> </div>
-                </div>
-                <div className="fila">
-                    <div className="seient"> </div>
-                    <div className="seient ocupat"> </div>
-                    <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
                 </div>
                 <div className="fila">
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient ocupat"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
                 </div>
                 <div className="fila">
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
                 </div>
                 <div className="fila">
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient ocupat"> </div>
                 </div>
                 <div className="fila">
-                    <div className="seient"> </div>
+                    <div className="seient ocupat"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient ocupat"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient ocupat"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                </div>
+                <div className="fila">
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
                     <div className="seient ocupat"> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                     <div className="seient ocupat"> </div>
-                    <div className="seient"> </div>
+                    <div className="seient" onClick={handleClick} style={{backgroundColor: activa ? "#6feaf6" : "#444451"}}> </div>
                 </div>
         </div>
      );
