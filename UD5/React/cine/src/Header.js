5105e68f22a2a5600666d8982d36013659cbc89b
PRobles_dwec <probles19088@iesjoanramis.org>
2023-02-06 16:15:39 +0100

    mejorando CSS

--- UD5/React/cine/src/Header.js
+++ UD5/React/cine/src/Header.js
@@ -2,8 +2,16 @@ const Header = () => {
   return (     
     <header>
       <div className="photoName">
-        <img  src="https://www.aficine.com/wp-content/uploads/2016/01/mao.jpg"/>  
-     </div>                                   
+        <img  src="https://www.aficine.com/wp-content/uploads/2016/01/mao.jpg"/>            
+     </div>  
+      <input type="search" />           
+      <button>Buscar</button>
+      <div>
+        <button>Inicio </button>
+        <button>Peliculas </button>
+        <button>Cines </button>
+        <button>Promociones </button>    
+      </div>                               
     </header>
   );
 }
