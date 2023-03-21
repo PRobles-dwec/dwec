b5bc613e3b5d8aebf4f07047c9842cb899128cfa
PRobles_dwec <probles19088@iesjoanramis.org>
2023-03-21 19:38:02 +0100

    quitando duplicados

--- UD4/Associacio-Futbol-Pau_Robles/js/script.js
+++ UD4/Associacio-Futbol-Pau_Robles/js/script.js
@@ -4,15 +4,20 @@ var primeraMasc = ["Almería", "Athletic Club", "Atlético de Madrid", "FC Barce
 // Array con los equipos de la segunda liga femenina.
 var segonaFem = ["Alavés", "Alhama", "Atlético Fem", "Barcelona Fem", "Tenerife", "Levante Fem", "Madrid CFF", "Betis Fem", "R.Madrid Fem", "R. Sociedad Fem", "Sevilla Fem", "Huelva Fem", "Valencia Fem", "Villarreal Fem"];
 
+// Array con los posibles días de los partidos.
 var diasPartidos = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]; // Array con los días de los partidos.
 
 const jornadesMasc = document.getElementById("jornadesMasc"); // Recogemos el elemento donde imprimiremos las jornadas masculinas
 const jornadesFem = document.getElementById("jornadesFem"); // Recogemos el elemento donde imprimiremos las jornadas femeninas
 
+const equips = { // Creamos un objeto para guardar los equipos.
+    primeraMasc: this.primeraMasc,
+    segonaFem: this.segonaFem,
+};
+console.log(equips);
+
 const resultatsMasc = document.getElementById("resultatsMasc"); // Recogemos el elemento donde imprimiremos los resultados masculinos.
 const resultatsFem = document.getElementById("resultatsFem"); // Recogemos el elemento donde imprimiremos los resultados femeninos.
-
-var tabla = document.getElementById("tabla");  // Recogemos el elemento en el que vamos a imprimir la tabla.
 
 function escriureJornades() {   // Función que escribe las jornadas masculinas
     var arrayJornades = []; // Aquí se guardarán las 14 jornadas.
@@ -52,14 +57,13 @@ function escriureJornades() {   // Función que escribe las jornadas masculinas
                     }
                                     
                     arrayJornades.push({local: aleatorioLocalFemenino, visitante: aleatorioVisitanteFemenino}); // Añadimos ambos equipos al array en dónde almacenamos las jornadas.            
-                                    
+                                       
                     localStorage.setItem("jornadasFemeninas", JSON.stringify(arrayJornades)); // Guardamos las variables en el LocalStorage.                               
 
                     jornadesFem.innerHTML += "<tr>" + "<td>" + aleatorioLocalFemenino + "</td>" + "<td>" + " - " + "</td>" + "<td>" + aleatorioVisitanteFemenino + "</td>" + "<td>" + getRandomHours() + "</td>" + "<td>" + diasPartidosAleatorio + "</td>"+ "</tr>"; // Añadimos la cadena y la imprimimos en el elemento dónde queremos imprimirlo.                                                   
-                }                                  
-            }    
-    
-    console.log(arrayJornades);   
+                }                                                              
+            }               
+    console.log(arrayJornades);     
 }
 
 function getRandom(min,max) { // Función para elegir un número aleatorio entre 2 números.
@@ -73,8 +77,8 @@ function getRandomHours() { // Función que hará que las horas sean aleatorias.
 }
 
 function escriureResultats() { // Función para escribir los resultados
-    var arrayResultats = []; // Array para guardar los resultados.
-
+    var arrayResultats = []; // Array para guardar los resultados.    
+    
     if(document.title == "Resultados Liga Masculina") { // Si el título de la página es el mismo.               
         resultatsMasc.style.display = "block";
         resultatsMasc.innerHTML = "<th>" + "Local" + "</th>" + "<th>" + "Resultado" + "</th>" + "<th>" + "Visitante" + "</th>"; // Creamos la cabecera de la tabla.
@@ -85,7 +89,6 @@ function escriureResultats() { // Función para escribir los resultados
             resultatsMasc.style.display = "none";
         }else {
             equipoMasculino.forEach((element, index)=> { // Recorremos cada uno de los elementos que están en el array de equipos masculinos.
-                console.log(element.local);  
                 var resultadoLocalMasculino = getRandom(0,6); // Variable que devuelve un número aleatorio entre 0 y 6, para el equipo local masculino.
                 var resultadoVisitanteMasculino = getRandom(0,6); // Variable que devuelve un número aleatorio entre 0 y 6, para el equipo visitante masculino.
 
@@ -118,10 +121,15 @@ function escriureResultats() { // Función para escribir los resultados
             });                
             localStorage.setItem("resultadosFemeninos", JSON.stringify(arrayResultats)); // Guardamos las variables en el LocalStorage. 
         }        
-    }  
+    }        
     if(arrayResultats.length == 0) {
         console.log("Array vacío.")
-    }          
+    }    
+    
+    const resultats = { // Creamos un objeto para guardar los resultados.
+        arrayResultats: arrayResultats,
+    };
+    console.log(resultats);       
 }
 
 function comprobarQuiniela() {
@@ -139,7 +147,7 @@ function comprobarQuiniela() {
     }else {
         var resultadosAux = resultadosAuxMasc.concat(resultadosAuxFem); // Creamos un nuevo array juntando los resultados.
         console.log(resultadosAux);
-        
+                
         resultadosAux.forEach((element, index) => { // Recorremos el array de los resultados.
             if(element.local > element.visitante) { // Si el resultado local, es mayor que el resultado visitante.
                 resultados_tabla.innerHTML += "<tr>"+ "<td>" + "1" + "</td>" + "</tr>"; // Imprimimos que es 1, haciendo referencia a que el local es mayor que el visitante.        
@@ -157,37 +165,141 @@ function comprobarQuiniela() {
         }else {
             alert("Tienes un total de 0 aciertos");
         }   
-    }          
+    }
+    const resultatsAux = {
+        resultadosAux: resultadosAux,
+    };
+    if(resultadosAux === undefined) {
+        console.log("Falta que generes resultados para que se guarden en el objeto de resultados.");
+    } else{
+        console.log(resultatsAux);
+    }    
 }
 
-function dibuixarTaulaPronostics() {    
-    document.addEventListener("DOMContentLoaded", function() { // Cuando la página cargue.
+function dibuixarTaulaPronostics() {      
+    var div = document.getElementById("contenedor_tabla");     
+    var tabla = document.createElement("table");
+    tabla.id = "pronosticos_tabla";
+    var tblBody = document.createElement("tbody");   
 
-    }, false);
-       
-    const pronosticos_tabla = document.getElementById("pronosticos_tabla"); // Recogemos el elemento en dónde imprimiremos la tabla.    
-
-    pronosticos_tabla.innerHTML = "<tr><th>" + "Jornadas" + "</th>" + "<th>" + "Partidos" + "</th>" + "<th>" + "Pronósticos" + "</th>" + "</tr>"; // Creamos la cabecera de la tabla.    
-    
     var jornadasAuxMasc = JSON.parse(localStorage.getItem("jornadasMasculinas")); // Recogemos los equipos masculinos del LocalStorage.
-    var jornadasAuxFem = JSON.parse(localStorage.getItem("jornadasFemeninas")); // Recogemos los equipos femeninos del LocalStorage.
-    
+    var jornadasAuxFem = JSON.parse(localStorage.getItem("jornadasFemeninas")); // Recogemos los equipos femeninos del LocalStorage.        
+
+    tabla.innerHTML = "<th>" + "Jornadas" + "</th>" +"<th>" + "Partidos" + "</th>" + "<th>" + "1" + "</th>" + "<th>" + "X" + "</th>" + "<th>" + "2" + "</th>";
     if(jornadasAuxMasc == null || jornadasAuxFem == null) {
         alert("Jornadas no disponibles. Pruebe más tarde.");
         console.log("Faltan generar jornadas.");
         pronosticos_tabla.style.display = "none";
-    }else {
+    } else {
+        // Creando las celdas 
         var jornadasAux = jornadasAuxMasc.concat(jornadasAuxFem); // Juntamos ambos arrays, para hacer uno nuevo.
-         jornadasAux.forEach((element, index) => { // Recorremos el nuevo array de jornadas.
-            console.log(element.local);
-            console.log(element.visitante);
-    
-            // Imprimimos los valores que deseamos recoger de dicho array.
-            pronosticos_tabla.innerHTML += "<tr>" + "<td>" + (index + 1) + "</td>" + "<td>" + element.local + " vs " + element.visitante +  "</td>" + 
-            "<td>" + "<input type='radio' name='"+index+"' value='input1' required> <input type='radio' name='"+index+"' value='inputX' required> <input type='radio' name='"+index+"' value='input2' required> </td>"+ "</tr>";
-        });   
-    }            
+        jornadasAux.forEach((element, index) => { // Recorremos el nuevo array de jornadas.
+            var fila = document.createElement("tr");
+            
+            var celda1 = document.createElement("td");
+            var celdaTexto1 = document.createTextNode(index+1);
+            celda1.appendChild(celdaTexto1);
+                        
+            var celda2 = document.createElement("td");
+            var celdaTexto2 = document.createTextNode(element.local + " vs " + element.visitante);
+            celda2.appendChild(celdaTexto2);
+            
+            for(let i = 0; i < 3; i++) {
+                var celda3 = document.createElement("td");
+                var celdaTexto3 = document.createElement("input");
+                celdaTexto3.type = "radio";
+                celdaTexto3.required = "true";
+
+                var celda4 = document.createElement("td");
+                var celdaTexto4 = document.createElement("input");
+                celdaTexto4.type = "radio";
+                celdaTexto4.required = "true";
+
+                var celda5 = document.createElement("td");
+                var celdaTexto5 = document.createElement("input");
+                celdaTexto5.type = "radio";
+                celdaTexto5.required = "true";
+            }
+
+            celda3.appendChild(celdaTexto3);
+
+            celda4.appendChild(celdaTexto4);
+            
+            celda5.appendChild(celdaTexto5);
+                                        
+            fila.appendChild(celda1);
+            fila.appendChild(celda2);
+            fila.appendChild(celda3);
+            fila.appendChild(celda4);
+            fila.appendChild(celda5);
+
+            tblBody.appendChild(fila);
+            tabla.appendChild(tblBody);
+
+            div.prepend(tabla);
+            
+        }); 
+        const jornadesAux = { // Creamos un objeto para guardar los resultados.
+            jornadasAux: jornadasAux,
+        };
+        if(jornadasAux === undefined) {
+            console.log("Falta que generes jornadas para guardarlas en el objeto dónde se guardan las jornadas.");        
+        } else {
+            console.log(jornadesAux); 
+        }          
+    }         
 }
-    if(document.title == "Apuestas Deportivas") { // Si el título del documento es igual.        
-        dibuixarTaulaPronostics(); // Llamamos a la función para dibujar la tabla de quinielas.
+
+function programacionDiasPartido() {
+    const numeroDiasPartido = this.teams.length - 1; // número de jornadas
+    const numeroPartidosPorDia = this.teams.length/2; // número de partidos por jornadas.
+    for(let i = 0; i < numeroDiasPartido; i++) {
+        const diaPartido = [];
+        for(let j = 0; j < numeroPartidosPorDia; j++) {
+            let partido = {local: visitante, visitante: visitante};
+            diaPartido.push(partido);
+        }
+        this.HorarioPartido.push(diaPartido);
     }
+}
+
+function equipoLocal() {
+    const nombresEquipo = this.teams.map (team => team.name);
+    const nombresEquiposLocales = this.teams.length - 1;
+    let indiceEquipo = this.teams.length - 1 -1;
+    this.HorarioPartido.forEach(diaPartido => {
+        diaPartido.forEach(function(partido, index) {
+            // Arrays siempre empiezan en 0.
+            if(index === 0) {
+                partido.local = nombresEquipo[nombresEquiposLocales];
+                // Para los demás partidos, que no son el primero.
+                partido.local = nombresEquipo[indiceEquipo];
+                indiceEquipo++;
+                console.log(partido.local);
+            }
+        });
+    }); 
+}
+
+function equipoVisitante() {
+    const nombresEquipo = this.teams.map (team => team.name);
+    const nombresEquiposVisitantes = this.teams.length - 1;
+    let indiceEquipo = this.teams.length - 1 -1;
+    this.HorarioPartido.forEach(diaPartido => {
+        diaPartido.forEach(function(partido, index) {
+            // Arrays siempre empiezan en 0.
+            if(index === 0) {
+                partido.visitante = nombresEquipo[nombresEquiposVisitantes];
+                // Para los demás partidos, que no son el primero.
+                partido.visitante = nombresEquipo[indiceEquipo];
+                indiceEquipo--;
+                console.log(partido.visitante);
+            }
+        });
+    }); 
+}
+console.log(programacionDiasPartido(partido.visitante));
+
+if(document.title == "Apuestas Deportivas") { // Si el título del documento es igual.        
+    dibuixarTaulaPronostics(); // Llamamos a la función para dibujar la tabla de quinielas.
+}
